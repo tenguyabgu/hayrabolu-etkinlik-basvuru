62054a7e004c745637a4eb37d4fc03a8ba47b4bc
tenguyabgu <brscnbigee07@gmail.com>
2026-09-21 21:17:47 +0300

    Set template folder and enable debug mode
    Updated Flask app to specify template folder and enable debug mode.

--- hayrabolu_etkinlik_basvuru/app.py
+++ hayrabolu_etkinlik_basvuru/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="Templates")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -34,8 +34,4 @@ def ana_sayfa():
 
 
 if __name__ == "__main__":
-
-    print("ÇALIŞAN KLASÖR:", os.getcwd())
-    print("LOGO VAR MI:", os.path.exists("static/images/belediye-logo.png"))
-
     app.run(debug=True)
